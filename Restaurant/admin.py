321a75e9bcfbc7d4af7cb5f31fd6674e0fce3915
Marc <mfett1@mail.com>
2023-01-22 08:09:18 -0500

    third commit

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    
+    list_display  = ('id', 'name', 'no_of_guests', 'booking_date')
+    ordering      = ['name', 'booking_date']
+    search_fields = ['name']
+   
+@admin.register(Menu)   
+class MenuAdmin(admin.ModelAdmin):
+    
+    list_display  = ('id', 'title', 'price', 'inventory')
+    ordering      = ['title', 'price']
+    search_fields = ['title']
+
+ 
